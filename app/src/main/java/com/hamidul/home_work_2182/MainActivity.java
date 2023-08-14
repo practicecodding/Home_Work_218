a073f89e8a5e09802cc1a4f5bf15348b6c3d3268
user <practicecoddin@gmail.com>
2023-08-14 09:07:11 +0600

    Home_Work_218.2

---
+++ app/src/main/java/com/hamidul/home_work_2182/MainActivity.java
@@ -0,0 +1,17 @@
+package com.hamidul.home_work_2182;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Home_Work_218.2
+
+    }
+}
