969c3f7a6c4f0ee4dcb48a2e03199a09e2333e55
user <practicecoddin@gmail.com>
2023-08-23 08:53:09 +0600

    Home_Work_218.1

--- app/src/main/java/com/hamidul/home_work_2181/MainActivity.java
+++ app/src/main/java/com/hamidul/home_work_2181/MainActivity.java
@@ -1,12 +1,24 @@
 package com.hamidul.home_work_2181;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView toolbar;
+    Animation toolbarAnimation;
+    TextView asset;
+    Button next;
+    EditText gold,silver,bank,hajj,out_of_loan,inbestments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +32,28 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         //======================================================================================================================
 
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        toolbar = findViewById(R.id.toolbar);
+        gold = findViewById(R.id.gold);
+        silver = findViewById(R.id.silver);
+        bank = findViewById(R.id.bank);
+        hajj = findViewById(R.id.hajj);
+        out_of_loan = findViewById(R.id.out_of_loan);
+        inbestments = findViewById(R.id.inbestments);
+        toolbarAnimation = AnimationUtils.loadAnimation(MainActivity.this,R.anim.toolbar_animation);
+        //=================================================================================================
+
+        toolbar.startAnimation(toolbarAnimation);
+
+        toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                toolbar.startAnimation(toolbarAnimation);
+
+            }
+        });
 
 
 
