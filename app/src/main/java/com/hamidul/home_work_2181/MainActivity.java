a6bc14fbaef38648c4a9f8ec8e3e0b4b7573b028
user <practicecoddin@gmail.com>
2023-08-26 22:18:57 +0600

    Home_Work_218.1

--- app/src/main/java/com/hamidul/home_work_2181/MainActivity.java
+++ app/src/main/java/com/hamidul/home_work_2181/MainActivity.java
@@ -24,11 +24,11 @@ import com.google.android.material.textfield.TextInputEditText;
 public class MainActivity extends AppCompatActivity {
     TextView toolbar;
     Animation toolbarAnimation;
-    TextView asset;
     Button next,result;
     EditText edGold,edSilver,edBank,edHajj,edOut_of_loan,edInvestments;
     EditText edCredit,edDue,edTax;
-    LinearLayout getAsset,getLiabilities;
+    LinearLayout getAsset,getLiabilities,getResult;
+    TextView tvResult;
     int back = 0;
 
     @Override
@@ -58,7 +58,8 @@ public class MainActivity extends AppCompatActivity {
         edDue = findViewById(R.id.due);
         edTax = findViewById(R.id.tax);
         result = findViewById(R.id.result);
-//        asset = findViewById(R.id.asset);
+        getResult  = findViewById(R.id.getResult);
+        tvResult = findViewById(R.id.tvResult);
         toolbarAnimation = AnimationUtils.loadAnimation(MainActivity.this,R.anim.toolbar_animation);
         //=================================================================================================
 
@@ -118,6 +119,70 @@ public class MainActivity extends AppCompatActivity {
         edDue.addTextChangedListener(Liabilities);
         edTax.addTextChangedListener(Liabilities);
 
+        result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int gold,goldTk,silver,silverTk,bank,hajj,out_of_loan,investments,credit,due,tax,asset,liabilities,result;
+
+                gold = 0;
+                silver = 0;
+                bank = 0;
+                hajj = 0;
+                out_of_loan = 0;
+                investments = 0;
+                credit = 0;
+                due = 0;
+                tax = 0;
+
+                if (edGold.getText().toString().length()>0) {
+                    gold = Integer.parseInt(edGold.getText().toString());
+                }
+                if (edSilver.getText().toString().length()>0) {
+                    silver = Integer.parseInt(edSilver.getText().toString());
+                }
+                if (edBank.getText().toString().length()>0) {
+                    bank = Integer.parseInt(edBank.getText().toString());
+                }
+                if (edHajj.getText().toString().length()>0) {
+                    hajj = Integer.parseInt(edHajj.getText().toString());
+                }
+                if (edOut_of_loan.getText().toString().length()>0) {
+                    out_of_loan = Integer.parseInt(edOut_of_loan.getText().toString());
+                }
+                if (edInvestments.getText().toString().length()>0) {
+                    investments = Integer.parseInt(edInvestments.getText().toString());
+                }
+                if (edCredit.getText().toString().length()>0) {
+                    credit = Integer.parseInt(edCredit.getText().toString());
+                }
+                if (edDue.getText().toString().length()>0) {
+                    due = Integer.parseInt(edDue.getText().toString());
+                }
+                if (edTax.getText().toString().length()>0) {
+                    tax = Integer.parseInt(edTax.getText().toString());
+                }
+
+                goldTk = gold*6442;
+                silverTk = silver*64;
+
+                asset = goldTk+silverTk+bank+hajj+out_of_loan+investments;
+                liabilities = credit+due+tax;
+
+                result = (asset-liabilities)/(40);
+
+                tvResult.setText("Your Payable Zakat \n\n"+result+" TK");
+
+                toolbar.setText("Result");
+
+                getLiabilities.setVisibility(View.GONE);
+                getResult.setVisibility(View.VISIBLE);
+
+                back = 4;
+
+            }
+        });
+
 
     }
 
@@ -137,7 +202,7 @@ public class MainActivity extends AppCompatActivity {
             String out_of_loan = edOut_of_loan.getText().toString();
             String inVestments = edInvestments.getText().toString();
 
-            next.setEnabled(!gold.isEmpty() && !silver.isEmpty() && !bank.isEmpty() && !hajj.isEmpty() && !out_of_loan.isEmpty() && !inVestments.isEmpty());
+            next.setEnabled(!gold.isEmpty() || !silver.isEmpty() || !bank.isEmpty() || !hajj.isEmpty() || !out_of_loan.isEmpty() || !inVestments.isEmpty());
 
             //next.setEnabled(gold.length() > 0 && silver.length() >0 && bank.length() >0 && hajj.length() >0 && out_of_loan.length() >0 && inbestments.length() >0);
 
@@ -166,7 +231,7 @@ public class MainActivity extends AppCompatActivity {
             String due = edDue.getText().toString();
             String tax = edTax.getText().toString();
 
-            result.setEnabled(!credit.isEmpty() && !due.isEmpty() && !tax.isEmpty());
+            result.setEnabled(!credit.isEmpty() || !due.isEmpty() || !tax.isEmpty());
 
         }
 
@@ -209,10 +274,27 @@ public class MainActivity extends AppCompatActivity {
             back = 3;
             getAsset.setVisibility(View.VISIBLE);
             getLiabilities.setVisibility(View.GONE);
+            getResult.setVisibility(View.GONE);
             toolbar.setText("Asset");
             toolbar.setTextColor(Color.parseColor("#673AB7"));
             toolbar.startAnimation(toolbarAnimation);
-        }else {
+        } else if (back==4) {
+            getAsset.setVisibility(View.VISIBLE);
+            getLiabilities.setVisibility(View.GONE);
+            getResult.setVisibility(View.GONE);
+            toolbar.setText("Asset");
+            toolbar.setTextColor(Color.parseColor("#673AB7"));
+            toolbar.startAnimation(toolbarAnimation);
+            edGold.setText("");
+            edSilver.setText("");
+            edBank.setText("");
+            edHajj.setText("");
+            edOut_of_loan.setText("");
+            edInvestments.setText("");
+            edCredit.setText("");
+            edDue.setText("");
+            edTax.setText("");
+        } else {
             super.onBackPressed();
         }
     }
